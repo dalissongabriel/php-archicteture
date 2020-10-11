a80cec9c1a590327cbff9e2269554c6ff341d89a
Alisson Gabriel <alissongabrieldino@gmail.com>
2020-10-11 17:43:35 -0300

    test: ensure that a student can have a phone

--- tests/StudentTest.php
+++ tests/StudentTest.php
@@ -6,6 +6,7 @@ namespace Alura\Tests;
 use Alura\Architecture\Cpf;
 use Alura\Architecture\Email;
 use Alura\Architecture\Student;
+use Alura\Architecture\Phone;
 use Alura\Architecture\Utils\Exceptions\InvalidCpfException;
 use Alura\Architecture\Utils\Exceptions\InvalidEmailException;
 
@@ -38,7 +39,16 @@ class StudentTest extends TestCase
     public function testMustEnsureThatTheEmailIsNotValid(){
         $this->expectException(InvalidEmailException::class);
         $student = new Student();
-        $student->setEmailAddress("anyinvalidemailsaddress");
+        $student->setEmailAddress("anyinvalidemailaddress");
+    }
+
+    public function testMustEnsureStudentHasAnPhone(){
+        $student = new Student();
+        $resultSet = $student->addPhone("49","988776543");
+        $resultGetPhone = $student->getPhone();
+        $this->assertSame("(49) 988776543", (string) $resultGetPhone);
+        $this->assertInstanceOf(Phone::class, $student->getPhone());
+        $this->assertInstanceOf(Student::class, $resultSet);
     }
 
 }
