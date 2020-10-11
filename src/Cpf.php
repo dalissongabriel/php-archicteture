5b0d513558031b67d64fc78d38127dc1019f0cc9
Alisson Gabriel <alissongabrieldino@gmail.com>
2020-10-11 16:25:12 -0300

    refactor: self encapsulation when setting cpf

--- src/Cpf.php
+++ src/Cpf.php
@@ -17,14 +17,19 @@ class Cpf
      */
     public function __construct(string $number)
     {
+        $this->setCpf($number);
+    }
+
+    private function setCpf(string $number): self
+    {
         if( !CpfValidator::isValid($number) ) {
             throw new InvalidCpfException($number);
         };
-
         $this->number = $number;
+        return $this;
     }
 
-    public function __toString()
+    public function __toString(): string
     {
        return (string) $this->number;
     }
