2fa46983612c6a9cfdb3890ac9aa430a07164c2f
Alisson Gabriel <alissongabrieldino@gmail.com>
2020-10-13 14:21:19 -0300

    test: ensure that a college can have a valid phone

--- tests/Unit/Domain/CollegeTest.php
+++ tests/Unit/Domain/CollegeTest.php
@@ -6,6 +6,11 @@ namespace Alura\Tests;
 
 use Alura\Architecture\Domain\College\College;
 
+
+use Alura\Architecture\Domain\Share\Phone;
+use Alura\Architecture\Utils\Exceptions\InvalidEmailException;
+
+use Alura\Architecture\Utils\Exceptions\InvalidPhoneException;
 use PHPUnit\Framework\TestCase;
 
 class CollegeTest extends TestCase
@@ -21,5 +26,39 @@ class CollegeTest extends TestCase
         $this->assertSame("Alura Cursos Tecnologia Ltda",$college->getSocialReason());
         $this->assertSame("Alura",$college->getFantasyName());
         $this->assertSame("alura@alura.com.br",(string) $college->getEmailAddress());
+    }
+
+    public function testMustEnsureCollegeHasPhone()
+    {
+        $college = College::withSocialReasonFantasyNameAndEmail(
+            "Alura Cursos Tecnologia Ltda",
+            "Alura",
+            "alura@alura.com.br")
+            ->addPhone("49","35529988");
+        $resultGetPhone = $college->getPhone();
+
+
+        $this->assertSame("(49) 35529988", (string) $resultGetPhone);
+        $this->assertInstanceOf(Phone::class, $college->getPhone());
+        $this->assertInstanceOf(College::class, $college);
+    }
+
+    public function testMustEnsureThatTheCpfIsNotValid()
+    {
+        $this->expectException(InvalidEmailException::class);
+        College::withSocialReasonFantasyNameAndEmail(
+            "Alura Cursos Tecnologia Ltda",
+            "Alura",
+            "alura"
+        );
+    }
+
+    public function testMustEnsureThatThePhoneIsNotValid(){
+        $this->expectException(InvalidPhoneException::class);
+        College::withSocialReasonFantasyNameAndEmail(
+            "Alura Cursos Tecnologia Ltda",
+            "Alura",
+            "alura@alura.com.brd")
+            ->addPhone("9","988776");
     }
 }
