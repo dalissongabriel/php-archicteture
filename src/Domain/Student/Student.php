6b1ec938f5b0cc14ac6e3c2ae9bfc22be9f0dede
Alisson Gabriel <alissongabrieldino@gmail.com>
2020-10-19 18:10:01 -0300

    feat: create inheritance class for people

--- src/Domain/Student/Student.php
+++ src/Domain/Student/Student.php
@@ -2,24 +2,17 @@
 
 namespace Alura\Architecture\Domain\Student;
 
-use Alura\Architecture\Domain\Share\Address;
 use Alura\Architecture\Domain\Share\Email;
-use Alura\Architecture\Domain\Share\Phone;
+use Alura\Architecture\Domain\Share\People;
+use Alura\Architecture\Domain\Share\Cpf;
 
-class Student
+class Student extends People
 {
-    private Cpf $cpf;
-    private string $name;
-    private Email $email;
-    private array $phones;
-    private Address $address;
-
-
     /**
      * Student named constructor  .
-     * @param Cpf $cpf
+     * @param string $cpf
      * @param string $name
-     * @param Email $email
+     * @param string $email
      */
     public static function withEmailCpfName(string $email, string $cpf, string $name): self
     {
@@ -31,78 +24,6 @@ class Student
         return $student;
     }
 
-    /**
-     * @param string $street
-     * @param int $number
-     * @param string $neighborhood
-     * @param string $city
-     * @param string $state
-     * @param string|null $compl
-     */
-    public function setAddress(string $street, int $number, string $neighborhood, string $city, string $state, string $compl = null): self
-    {
-        $this->address = new Address();
-        $this->address->setStreet($street);
-        $this->address->setNeighborhood($neighborhood);
-        $this->address->setCity($city);
-        $this->address->setState($state);
-        $this->address->setNumber($number);
-        if( $compl !== null ) {
-            $this->address->setCompl($compl);
-        }
-        return $this;
-
-    }
-
-    public function getAddress(): Address
-    {
-        return $this->address;
-    }
-
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-        return $this;
-    }
-
-    public function setEmailAddress(string $address): self
-    {
-        $this->email = new Email($address);
-        return $this;
-    }
-
-    public function getEmailAddress(): Email
-    {
-        return $this->email;
-    }
-
-    public function setCpf(string $cpf): self
-    {
-        $this->cpf = new Cpf($cpf);
-        return $this;
-    }
-
-    public function getCpf(): Cpf
-    {
-        return $this->cpf;
-
-    }
-    public function addPhone($ddd, $number): self
-    {
-        $this->phones[] = new Phone($ddd,$number);
-        return $this;
-    }
-
-    public function getPhone(): Phone
-    {
-        return $this->phones[0];
-    }
-
-    public function getPhones(): array
-    {
-        return $this->phones;
-    }
 
     public function __toString(): string
     {
